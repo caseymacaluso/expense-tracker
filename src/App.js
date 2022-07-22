f427522fd2b3fda974bcd4b5bbf9877e07c88b28
Casey Macaluso <macaluso.casey@gmail.com>
2022-07-22 17:20:39 -0500

    add ExpenseItem and ExpenseDate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import ExpenseItem from "./components/ExpenseItem";
+import { expenses } from "./constants";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>another one</p>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
     </div>
   );
 }
