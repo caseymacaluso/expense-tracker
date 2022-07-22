2ae7babbf3dfda8119098210aa3b629d8bc13e8f
Casey Macaluso <macaluso.casey@gmail.com>
2022-07-22 18:03:03 -0500

    add Expenses, Card components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 import { expenses } from "./constants";
 
 function App() {
-  return (
-    <div>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      />
-    </div>
-  );
+  return <Expenses items={expenses} />;
 }
 
 export default App;
