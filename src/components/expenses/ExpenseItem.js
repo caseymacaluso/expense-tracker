61fc6f8b589b1395d7195d9f0271c26cdf473128
Casey Macaluso <macaluso.casey@gmail.com>
2022-07-22 21:44:42 -0500

    add click listener, useState on expenseItem

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
 // Component for an individual expense item for the expense tracker
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
 
 const ExpenseItem = props => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!!!!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click me</button>
     </Card>
   );
 };
